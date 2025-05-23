d9f06523fa0e04aa664780aaceef0a2b021f63f3
fholfeld <thedareias@gmail.com>
2025-05-24 00:19:16 +0200

    fixed lstnew and .h file

--- libft.h
+++ libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fholfeld <fholfeld@student.42.fr>          +#+  +:+       +#+        */
+/*   By: fillip <fillip@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 20:20:02 by fillip            #+#    #+#             */
-/*   Updated: 2025/05/23 14:29:01 by fholfeld         ###   ########.fr       */
+/*   Updated: 2025/05/23 21:34:15 by fillip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,5 +77,19 @@ void	ft_putstr_fd(const char *s, const int fd);
 void	ft_putendl_fd(const char *s, const int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(const char *s, const int fd);
+/*
+** ***************************************************************************
+**								LINKED LIST
+** ***************************************************************************
+*/
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *nlst);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list	*lst);
+void	ft_lstadd_back(t_list **lst, t_list *nlst);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
